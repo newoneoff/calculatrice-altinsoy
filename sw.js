95801c3e1103be2af69b6e7bafcad35d5b92df89
newoneoff <NewOnecigkofte@hotmail.com>
2026-06-06 19:02:02 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('calc-cache').then(cache =>
-      cache.addAll(['./', './index.html', './css.css', './js.js', './unnamed (2).png'])
+      cache.addAll(['./', './index.html', './css.css', './js.js', './logo-calculatrice.png'])
 
     )
   );
